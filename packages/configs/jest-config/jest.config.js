4dd521180d969d357aad05c015f99582d2ef4641
Gustavo Trentini <gustavo.trentini@universo.univates.br>
2021-10-06 13:33:41 -0300

    Ajustes jest

--- packages/configs/jest-config/jest.config.js
+++ packages/configs/jest-config/jest.config.js
@@ -1,36 +1,30 @@
 module.exports = {
   clearMocks: true,
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  rootDir: '.',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  rootDir: ".",
   testTimeout: 20000,
   testPathIgnorePatterns: [
-    '<rootDir>/node_modules',
-    '<rootDir>/(?:.+?)/node_modules/'
+    "<rootDir>/node_modules",
+    "<rootDir>/(?:.+?)/node_modules/",
   ],
-  coveragePathIgnorePatterns:[
-    '<rootDir>/aws'
-  ],
-  moduleFileExtensions: [
-    'js',
-    'json',
-    'ts'
-  ],
+  coveragePathIgnorePatterns: ["<rootDir>/aws"],
+  moduleFileExtensions: ["js", "json", "ts"],
   transform: {
-    '.+\\.(t|j)s$': 'ts-jest'
+    ".+\\.(t|j)s$": "ts-jest",
   },
-  cacheDirectory: '.jest/.cache',
+  cacheDirectory: ".jest/.cache",
   collectCoverage: true,
-  coverageDirectory: '.jest/coverage',
+  coverageDirectory: ".jest/coverage",
   coverageThreshold: {
     global: {
       branches: 50,
       functions: 50,
       lines: 50,
-      statements: 50
-    }
+      statements: 50,
+    },
   },
-  collectCoverageFrom: [
-    '<rootDir>/**/*.service.ts'
-  ]
-}
+  collectCoverageFrom: ["<rootDir>/**/*.service.ts"],
+  projects: ["<rootDir>/packages/**/jest.config.js"],
+  testMatch: ["*.spec.ts", "*.spec.tsx"],
+};
