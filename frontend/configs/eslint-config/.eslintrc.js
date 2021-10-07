7ea79016edceabee6144b9badee230bd14302ee4
Gustavo Trentini <gustavo.trentini@universo.univates.br>
2021-10-07 10:02:55 -0300

    feat: :art: testando commit
    testando

--- frontend/configs/eslint-config/.eslintrc.js
+++ frontend/configs/eslint-config/.eslintrc.js
@@ -14,22 +14,17 @@ module.exports = {
     node: true,
     jest: true
   },
-  extends: [
-    'standard',
-    'plugin:react/recommended',
-    'react-app'
-  ],
-  plugins: [
-    'react',
-    'import-helpers',
-    'modules-newline'
-  ],
+  extends: ['standard', 'plugin:react/recommended', 'react-app'],
+  plugins: ['react', 'import-helpers', 'modules-newline'],
   rules: {
     'template-curly-spacing': [2, 'always'],
     'jsx-quotes': ['error', 'prefer-double'],
     'react/display-name': 'off',
     'react/forbid-prop-types': 'off',
-    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'react/function-component-definition': [
+      2,
+      { namedComponents: 'arrow-function' }
+    ],
     'react/jsx-curly-spacing': [2, 'always'],
     'react/jsx-filename-extension': 0,
     'react/jsx-props-no-spreading': 0,
@@ -37,48 +32,65 @@ module.exports = {
     'react/jsx-no-duplicate-props': [2, { ignoreCase: false }],
     'react/react-in-jsx-scope': 'off',
     'react/require-default-props': 'error',
-    'react-hooks/exhaustive-deps': ['warn', { additionalHooks: '(useFormEffect)' }],
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      { additionalHooks: '(useFormEffect)' }
+    ],
     'react/no-unused-prop-types': 'error',
-    'import-helpers/order-imports': ['error', {
-      newlinesBetween: 'always',
-      groups: [
-        '/^react/',
-        '/^redux/',
-        'module',
-        '/^lodash/',
-        '/^@material-ui/',
-        '/^@britania-crm/',
-        '/^~/',
-        ['parent', 'sibling', 'index']
-      ],
-      alphabetize: {
-        order: 'asc',
-        ignoreCase: true
+    'import-helpers/order-imports': [
+      'error',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          '/^react/',
+          '/^redux/',
+          'module',
+          '/^lodash/',
+          '/^@material-ui/',
+          '/^@britania-crm/',
+          '/^~/',
+          ['parent', 'sibling', 'index']
+        ],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true
+        }
       }
-    }],
+    ],
     'import/no-extraneous-dependencies': 0,
     'import/prefer-default-export': 0,
     indent: ['warn', 2, { SwitchCase: 1 }],
     'arrow-body-style': ['error', 'as-needed'],
-    'no-unused-vars': ['error', {
-      vars: 'all',
-      args: 'after-used',
-      ignoreRestSiblings: true
-    }],
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true
+      }
+    ],
     'generator-star-spacing': ['error', { before: false, after: true }],
     'space-before-function-paren': ['error', 'always'],
-    'object-curly-newline': ['error', {
-      ObjectExpression: { multiline: true, minProperties: 3 },
-      ObjectPattern: { multiline: true, minProperties: 3 },
-      ImportDeclaration: { multiline: true, minProperties: 3 },
-      ExportDeclaration: { multiline: true, minProperties: 3 }
-    }],
-    'object-shorthand': ['error', 'always', { avoidExplicitReturnArrows: true }],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: { multiline: true, minProperties: 3 },
+        ObjectPattern: { multiline: true, minProperties: 3 },
+        ImportDeclaration: { multiline: true, minProperties: 3 },
+        ExportDeclaration: { multiline: true, minProperties: 3 }
+      }
+    ],
+    'object-shorthand': [
+      'error',
+      'always',
+      { avoidExplicitReturnArrows: true }
+    ],
     'modules-newline/import-declaration-newline': 'error',
     'modules-newline/export-declaration-newline': 'error',
     'arrow-parens': ['error', 'always'],
     'import/no-unresolved': 2,
-    'prefer-template': 'error'
+    'prefer-template': 'error',
+    'eslint key-spacing': [2, { beforeColon: true, afterColon: true }]
   },
   settings: { react: { version: 'detect' } },
   globals: { __DEV__: true }
