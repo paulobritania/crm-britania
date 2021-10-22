ee8bacf8104004501824a088d507dc4fed66dbc8
rafatrinity <rafaeltrindade649@gmail.com>
2021-10-22 15:18:25 -0300

    feat: :sparkles: update and create line families

--- backend/packages/services-svc/src/modules/buyers/dto/update/updateBuyerLineFamily.ts
+++ backend/packages/services-svc/src/modules/buyers/dto/update/updateBuyerLineFamily.ts
@@ -1,10 +1,53 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsNumber } from 'class-validator'
+import { Type } from 'class-transformer'
+import { IsDate, IsNotEmpty, IsNumber, IsString, Length } from 'class-validator'
+import { CreateBuyerDto } from '../create/createBuyer.dto'
 
 import { CreateBuyerLineFamily } from '../create/createBuyerLineFamily.dto'
 
+const required = { required: true }
 export class UpdateBuyerLineFamily extends CreateBuyerLineFamily {
+  @IsString()
+  @Length(12, 12)
+  @IsNotEmpty()
+  @ApiProperty(required)
+  familyCode: string
+
+  @IsString()
+  @Length(3, 200)
+  @IsNotEmpty()
+  @ApiProperty(required)
+  familyDescription: string
+
   @IsNumber()
-  @ApiProperty({ required: false })
-  id?: number
+  @IsNotEmpty()
+  @ApiProperty(required)
+  lineCode: number
+
+  @IsString()
+  @Length(3, 200)
+  @IsNotEmpty()
+  @ApiProperty(required)
+  lineDescription: string
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty(required)
+  regionalManagerCode: number
+
+  @IsString()
+  @Length(3, 70)
+  @IsNotEmpty()
+  @ApiProperty(required)
+  regionalManagerDescription: string
+
+  @IsDate()
+  @IsNotEmpty()
+  @ApiProperty(required)
+  updatedAt: Date
+
+  @Type(() => CreateBuyerDto)
+  @ApiProperty({ ...required, type: CreateBuyerDto })
+  buyer: CreateBuyerDto
+
 }
